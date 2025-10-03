a1c6059cae7bfaa2ac3129ea254af6219e0db49b
Aleksandra Kvashko <alex.kvashko@mail.ru>
2025-10-03 16:53:58 +0300

    Lab 3: Task 2 - list comprehension even numbers

--- lab3.py
+++ lab3.py
@@ -3,3 +3,7 @@ print("=== Задание 1 ===")
 squares = [x**2 for x in range(1, 11)] 
 print("Квадраты чисел:", squares) 
 print() 
+print("=== Задание 2 ===") 
+even_numbers = [x for x in range(1, 21) if x % 2 == 0] 
+print("Четные числа:", even_numbers) 
+print() 
